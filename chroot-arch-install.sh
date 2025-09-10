93f56d751a71e127aa69fbe4f2e2a1cdfbe569a8
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-10 15:49:17 -0300

    feat: adicionado networkmanager e aviso sobre o sudo

--- chroot-arch-install.sh
+++ chroot-arch-install.sh
@@ -27,7 +27,12 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Instalação com chroot finalizada" 
+# NetworkManager
+pacman -S networkmanager
+systemctl enable NetworkManager
+
+echo "Instalação com chroot finalizada"
+echo "Usuário deve ser adicionado ao sudo!"
 
 
 
