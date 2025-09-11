f13208726640c012e9c86f03e21b937f1e3aec52
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-11 09:12:52 -0300

    feat: instalação de pacotes e configuração do xorg-xinit

---
+++ post-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks j4-dmenu-desktop feh chromium thunar ttf-ibm-plex
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+echo "Adicionar i3 ao .xinitrc"
