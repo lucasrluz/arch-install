ea8de3b17798a2ec9944e21e550ae1df2811af6b
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-11 14:09:46 -0300

    feat: alteração no layout das partições

--- arch-install.sh
+++ arch-install.sh
@@ -7,27 +7,27 @@ parted -s /dev/sda mklabel gpt
 parted -s /dev/sda mkpart primary fat32 1MiB 1025MiB
 parted -s /dev/sda set 1 esp on
 
-# Partição para o sistema
-parted -s /dev/sda mkpart primary ext4 1025MiB 507844MiB
-
 # Partição para o swap
-parted -s /dev/sda mkpart primary linux-swap 507844MiB 100%
+parted -s /dev/sda mkpart primary linux-swap 1025MiB 5GiB
+
+# Partição para o sistema
+parted -s /dev/sda mkpart primary ext4 5GiB 100%
 
 sleep 1
 
 # Formatação
 mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
-mkswap /dev/sda3
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
 
 sleep 1
 
 # Monta as partições
-mount /dev/sda2 /mnt
+mount /dev/sda3 /mnt
 
 mount --mkdir /dev/sda1 /mnt/boot/efi
 
-swapon /dev/sda3
+swapon /dev/sda2
 
 sleep 1
 
