6b8018a4275f6972a26f0554260676ac1354c52a
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-11 09:29:29 -0300

    feat: adicionado instalação do fish e gcc

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty feh chromium thunar ttf-ibm-plex
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
