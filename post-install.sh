7e3d9253eb1b05ef0f366459bb85ce19ac990777
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-11 09:35:08 -0300

    feat: adicionado arquivos de configuração no .config

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,10 @@
 
 sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc
 
+git clone https://github.com/lucasrluz/dotfiles.git
+
+mv dotfiles/.config/ ~/
+
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 echo "Adicionar i3 ao .xinitrc"
