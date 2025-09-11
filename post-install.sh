c68f8b666b484818c0f11ed3e2b5e578f04c70dd
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-09-11 09:23:36 -0300

    feat: adicionado instalação do alacritty

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks j4-dmenu-desktop feh chromium thunar ttf-ibm-plex
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks j4-dmenu-desktop alacritty feh chromium thunar ttf-ibm-plex
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
