c77dd2d247139d81cc6c54e499f9a884070fbffe
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-11-27 08:58:15 -0300

    feat: adicionado TLP

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc acpi
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc acpi tlp
 
 git clone https://github.com/lucasrluz/dotfiles.git
 git clone https://github.com/lucasrluz/wallpaper.git
@@ -11,5 +11,9 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 sudo systemctl enable fstrim.timer
 sudo systemctl start fstrim.timer
+
+sudo systemctl enable tlp.service
+sudo systemctl start tlp.service
+sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
 
 echo "Adicionar i3 ao .xinitrc"
