4d61b37dde83591f4f1ad738da8c51fa5a3be086
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-11-27 08:54:37 -0300

    feat: adicionado instalação de pacote e clone de repositório

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc acpi
 
 git clone https://github.com/lucasrluz/dotfiles.git
+git clone https://github.com/lucasrluz/wallpaper.git
 
 mv dotfiles/.config/ ~/
 
